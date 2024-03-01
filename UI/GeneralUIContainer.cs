656a7df2b7a1772d23749be938e20e4a408ebac4
Mineant <kawaiianthony@gmail.com>
2024-03-01 14:05:36 +0800

    added general ui container

---
+++ UI/GeneralUIContainer.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mineant
+{
+    public class GeneralUIContainer : Container<GeneralUIProduct, GeneralUIProductArgs>
+    {
+
+    }
+
+}
