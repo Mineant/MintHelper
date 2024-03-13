5af0d432fbf172aaa17556eec7efeb69bcc199c4
Mineant <kawaiianthony@gmail.com>
2024-03-13 20:41:48 +0800

    Added TryGetUnityObjectsOfTypeFromPath to helpers

--- General/Helpers.cs
+++ General/Helpers.cs
@@ -350,6 +350,48 @@ namespace Mineant
             return interfaces;
         }
 
+#if UNITY_EDITOR
+
+        /// <summary>
+        /// Adds newly (if not already in the list) found assets.
+        /// Returns how many found (not how many added)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="path"></param>
+        /// <param name="assetsFound">Adds to this list if it is not already there</param>
+        /// <returns></returns>
+        public static int TryGetUnityObjectsOfTypeFromPath<T>(string path, List<T> assetsFound) where T : UnityEngine.Object
+        {
+            string[] filePaths = System.IO.Directory.GetFiles(path);
+
+            int countFound = 0;
+
+            Debug.Log(filePaths.Length);
+
+            if (filePaths != null && filePaths.Length > 0)
+            {
+                for (int i = 0; i < filePaths.Length; i++)
+                {
+                    UnityEngine.Object obj = UnityEditor.AssetDatabase.LoadAssetAtPath(filePaths[i], typeof(T));
+                    if (obj is T asset)
+                    {
+                        countFound++;
+                        if (!assetsFound.Contains(asset))
+                        {
+                            assetsFound.Add(asset);
+                        }
+                    }
+                }
+            }
+
+            return countFound;
+        }
+
+#endif
+
+
+
+
         /// <summary>
         /// This is useful for just picking one element without the need to check unique or anything
         /// </summary>
